edc8e4f53050ba13db766d28f14f389543882d4e
App1epli <alistynyshtyk07@gmail.com>
2026-01-31 00:19:43 +0500

    commit

--- src/week_1/task_5.java
+++ src/week_1/task_5.java
@@ -1,10 +1,19 @@
 package week_1;
-
+import java.util.Scanner;
 public class task_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int balance = 0;
+		System.out.println("Your current balance is: " + balance);
+		System.out.println("Please enter your money: ");
+		
+		Scanner scaner = new Scanner(System.in);
+		
+		int money = scaner.nextInt();
+		balance += money;
+		
+		System.out.println("You inserted" + money + "KZT, Your curent balance is " + balance);
+		
 	}
 
 }
--- src/week_1/task_6.java
+++ src/week_1/task_6.java
@@ -1,10 +1,32 @@
 package week_1;
+import java.util.Scanner;
 
 public class task_6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("enter your string: ");
+		Scanner scaner = new Scanner(System.in);
+		String s = scaner.nextLine();
+		
+		int left = 0;
+		int right = s.length() - 1;
+		boolean ispali = true;
+		
+		while(left < right) {
+			if(s.charAt(left) != s.charAt(right)){
+				ispali = false;
+				break;
+			}
+			left++;
+			right--;
+		}
+		
+		if(ispali) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
 	}
 
 }
